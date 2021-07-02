9c2cd371622d7ccdfc0320f13f8ce93ca46e2b15
Atlas <jack@bentsen.us>
2021-07-02 16:40:23 -0700

    added ip checker api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,35 @@
-// OPEN WEATHER API CALL REFERENCE 
+// Grabbing visitor IP address...
+var userLoc 
+fetch("https://ipinfo.io/json?token=5607825501f064").then(
+    (response) => response.json()
+).then( (data) => {
+    console.log(data);
+    currentCityNameDisplay.text(data.city);
+    userLoc = data.loc;
+    console.log(userLoc);
+})
 
-// API KEY: c11639f11059953c1817728af454e744
 
-// https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
 
-// On page load, update weather from localStorage items
-// ie: pull the x-5 index localStorage items to get the previous 5
-// If none wait for field input
+
+// DOM Nodes...
+var currentCityNameDisplay = $("#cityNameDisplay")
+var currentCityTemp = $("#temp")
+var currentCityWind = $("#wind")
+var currentCityHumidity = $("#humidity")
+var currentCityUV = $("#uv")
+
+// City Lat/Lon holders for search api response
+
+
+// Check local storage for search info, pull latest key and fetch current weather data for previously searched entry
+if (localStorage.length > 0) {
+
+}
+// Stringholder for Fetch
+var fetchForm = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&appid=c11639f11059953c1817728af454e744"
 
 let x = localStorage.length
-console.log(x)
 $("#btn").submit(function(event) {
     event.preventDefault();
     localStorage.setItem(x, $("#searchInput").val());
