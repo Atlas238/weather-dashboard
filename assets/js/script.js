4a8f52ebb9e003ba0d442b6e5903ddd7f9f18f1c
Atlas <jack@bentsen.us>
2021-07-03 11:45:40 -0700

    fixed city name geocoder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Media query for Icon movement
-navigator.geolocation.getCurrentPosition(position => {
-    console.log(position.coords)
-})
+navigator.geolocation.getCurrentPosition((position) => {
+  console.log(position);
+});
 
 // DOM Nodes...
 var currentCityNameDisplay = $("#cityNameDisplay");
@@ -15,49 +15,55 @@ var weatherIconAdjustHolder = $("#weatherIconAdjust");
 // Grabbing visitor IP address...
 
 var userLoc;
+var windowWidth = window.matchMedia("(max-width:780px)");
 
 function getUserIP() {
-  fetch("https://ipinfo.io/json?token=5607825501f064")
-    .then((response) => response.json())
-    .then((responseJSON) => {
-      console.log(responseJSON);
-      currentCityNameDisplay.text(responseJSON.city);
-      userLoc = responseJSON.loc;
-      console.log(userLoc);
-      var latLon = userLoc.split(",");
-      fetch(
-        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-          latLon[0] +
-          "&lon=" +
-          latLon[1] +
-          "&units=imperial&&exclude=hourly,minutely&appid=c11639f11059953c1817728af454e744"
-      )
-        .then((response) => response.json())
-        .then((responseData) => {
-          // Take response data for Current City (From IP location) and fill placeholder elements with corresponding data
-          // First block handles main display
-          console.log(responseData);
-          currentCityTemp.text(responseData.current.temp);
-          currentCityWind.text(responseData.current.wind_speed);
-          currentCityHumidity.text(responseData.current.humidity);
-          currentCityUV.text(responseData.current.uvi);
-          currentCityWeatherIcon.attr(
-            "src",
-            "http://openweathermap.org/img/wn/" +
-              responseData.current.weather[0].icon +
-              "@4x.png"
-          );
-          weatherIconAdjustHolder.attr(
-            "src",
-            "http://openweathermap.org/img/wn/" +
-              responseData.current.weather[0].icon +
-              "@4x.png"
-          );
+  // Asks for location and pulls current location then runs a fetch request at given lat long values
+  navigator.geolocation.getCurrentPosition((position) => {
+    fetch(
+      "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=" +
+        position.coords.latitude +
+        "&longitude=" +
+        position.coords.longitude +
+        "&localityLanguage=en"
+    )
+      .then((response) => response.json())
+      .then((responseCityData) => {
+        currentCityNameDisplay.text(responseCityData.city);
+      });
 
-          // Daily forecast Tiles
+    fetch(
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        position.coords.latitude +
+        "&lon=" +
+        position.coords.longitude +
+        "&units=imperial&exclude=hourly,minutely&appid=c11639f11059953c1817728af454e744"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+        // First block handles main display
+        currentCityNameDisplay.text(responseData.city);
+        currentCityTemp.text(responseData.current.temp);
+        currentCityWind.text(responseData.current.wind_speed);
+        currentCityHumidity.text(responseData.current.humidity);
+        currentCityUV.text(responseData.current.uvi);
+        currentCityWeatherIcon.attr(
+          "src",
+          "http://openweathermap.org/img/wn/" +
+            responseData.current.weather[0].icon +
+            "@4x.png"
+        );
+        weatherIconAdjustHolder.attr(
+          "src",
+          "http://openweathermap.org/img/wn/" +
+            responseData.current.weather[0].icon +
+            "@4x.png"
+        );
+      });
+  });
 
-        });
-    });
+  // Daily forecast Tiles
 }
 
 getUserIP();
@@ -66,12 +72,12 @@ getUserIP();
 if (localStorage.length > 0) {
 }
 // Stringholder for Fetch
-var fetchForm =
-  "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-  cityLat +
-  "&lon=" +
-  cityLon +
-  "&units=imperial&appid=c11639f11059953c1817728af454e744";
+// var fetchForm =
+//   "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+//   cityLat +
+//   "&lon=" +
+//   cityLon +
+//   "&units=imperial&appid=c11639f11059953c1817728af454e744";
 
 let x = localStorage.length;
 $("#btn").submit(function (event) {
@@ -79,9 +85,3 @@ $("#btn").submit(function (event) {
   localStorage.setItem(x, $("#searchInput").val());
   x++;
 });
-
-fetch(
-  `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=imperial&appid=c11639f11059953c1817728af454e744`
-)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
